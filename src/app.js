7828ea7d0ab52b1f3756f1a95dc090c6977cefb0
Jintops <jintops2003@gmail.com>
2025-09-16 02:54:31 +0530

    userSchema

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const connectDB=require('./config/database')
 const app=express();
 
 connectDB().then(()=>{
-
     console.log("Database connected success")
     app.listen(7777,()=>{
     console.log("Server successfully connected hE2AGS5NX4dO7eo3")
